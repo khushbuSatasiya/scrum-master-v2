3370ab247850d756b270f554277b8248003d50cd
khushbuSatasiya <khushbu.satasiya@scaletech.xyz>
2023-08-29 17:35:12 +0530

    fix: scroll area issue

--- src/shared/hoc/layout/container/layout.tsx
+++ src/shared/hoc/layout/container/layout.tsx
@@ -16,24 +16,20 @@ const Layout: React.FC<PropsWithChildren> = (props) => {
   const theme = useMantineTheme();
   const [isOpened, setIsOpened] = useState(false);
 
-   
-
-
   return (
     <AppShell
       padding={0}
       navbarOffsetBreakpoint="sm"
-      asideOffsetBreakpoint="sm"  
+      asideOffsetBreakpoint="sm"
     >
-      {/* <ScrollArea
-      type="never"
+      <ScrollArea
+        type="never"
         sx={{
           backgroundColor: "#F0F3F4",
           height: "100%",
-       
         }}
         scrollbarSize={2}
-      > */}
+      >
         <Box p="md" pb={0}>
           <Flex align={"center"} justify="space-between">
             <MediaQuery largerThan="sm" styles={{ display: "none" }}>
@@ -46,12 +42,11 @@ const Layout: React.FC<PropsWithChildren> = (props) => {
               />
             </MediaQuery>
             <Space w="md" />
-           
           </Flex>
         </Box>
         {/* <Breadcrumb /> */}
         {props.children}
-      {/* </ScrollArea> */}
+      </ScrollArea>
     </AppShell>
   );
 };
